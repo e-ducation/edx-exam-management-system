7a54b0302a86dc985af2a2e4629289229bc884e9
weijie <wejie00@foxmail.com>
2018-09-05 13:55:20 +0800

    Add exam paper api

--- ems/urls.py
+++ ems/urls.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 """ems URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
+from __future__ import unicode_literals
+
+from django.conf.urls import url, include
 from django.contrib import admin
 
-urlpatterns = [
+from auth_backends.urls import auth_urlpatterns
+from django.views.generic import TemplateView
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Exam Management System API",
+        default_version='v1',
+        description="Exam Management System API",
+        contact=openapi.Contact(email="code@e-ducation.cn"),
+        license=openapi.License(name="AGPL-3.0 License"),
+    ),
+    validators=['flex', 'ssv'],
+)
+
+urlpatterns = auth_urlpatterns + [
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('exam_paper.api.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
---
+++ exam_paper/models.py
@@ -0,0 +1,63 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.db.models import Sum
+
+from model_utils.models import TimeStampedModel
+from jsonfield import JSONField
+
+
+PAPER_CREATE_TYPE = (
+    ('fixed', 'fixed'),
+    ('random', 'random')
+)
+
+PROBLEM_TYPE = (
+    ('multiplechoiceresponse', 'single_choice'),
+    ('choiceresponse', 'multi_choice'),
+    ('stringresponse', 'fill_in'),
+)
+
+
+class ExamPaper(TimeStampedModel):
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500, blank=True)
+    create_type = models.CharField(max_length=16, choices=PAPER_CREATE_TYPE, default='fixed')
+    passing_ratio = models.IntegerField(default=60,
+                                        validators=[MinValueValidator(1), MaxValueValidator(100)])
+    creator = models.ForeignKey(User, db_index=True)
+
+    @property
+    def total_problem_num(self):
+        return self.problems.count()
+
+    @property
+    def total_grade(self):
+        result = self.problems.aggregate(total_grade=Sum('grade'))
+        return result and result.get('total_grade') or 0
+
+
+class ExamPaperProblems(TimeStampedModel):
+
+    exam_paper = models.ForeignKey(ExamPaper, related_name='problems', null=True)
+    sequence = models.CharField(max_length=16, default='01')
+    problem_id = models.CharField(max_length=255, db_index=True)
+    problem_type = models.CharField(max_length=16, choices=PROBLEM_TYPE)
+    grade = models.DecimalField(max_digits=5, decimal_places=2,
+                                validators=[MinValueValidator(0.01), MaxValueValidator(100.00)])
+    content = JSONField()
+
+
+class ExamPaperCreateRule(TimeStampedModel):
+
+    exam_paper = models.ForeignKey(ExamPaper, related_name='rules', null=True)
+    problem_section_id = models.CharField(max_length=255, null=True, blank=True)
+    section_name = models.CharField(max_length=255)
+    problem_type = models.CharField(max_length=16, choices=PROBLEM_TYPE)
+    problem_num = models.IntegerField(default=0)
+    grade = models.DecimalField(max_digits=5, decimal_places=2, default=1.00,
+                                validators=[MinValueValidator(0.01), MaxValueValidator(100.00)])
