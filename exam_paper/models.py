9d5af8e2a77e3f967542b241ed7dcac5b82ed256
weijie <wejie00@foxmail.com>
2018-09-18 14:09:39 +0800

    Add exam task

--- exam_paper/models.py
+++ exam_paper/models.py
@@ -23,6 +23,18 @@ PROBLEM_TYPE = (
     ('stringresponse', 'fill_in'),
 )
 
+TASK_STATE = (
+    ('pending', 'pending'),
+    ('started', 'started'),
+    ('finished', 'finished'),
+)
+
+EXAM_RESULT = (
+    ('pass', 'pass'),
+    ('flunk', 'flunk'),
+    ('pending', 'pending'),
+)
+
 
 class ExamPaper(TimeStampedModel):
 
@@ -67,3 +79,55 @@ class ExamPaperCreateRule(TimeStampedModel):
                                 validators=[
                                     MinValueValidator(Decimal((0, (0, 0, 1), -2))),
                                     MaxValueValidator(Decimal(100))])
+
+
+class ExamTask(TimeStampedModel):
+
+    exampaper_name = models.CharField(max_length=50)
+    exampaper_description = models.CharField(max_length=500, blank=True)
+    exampaper_create_type = models.CharField(max_length=16, choices=PAPER_CREATE_TYPE)
+    exampaper_passing_ratio = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    creator = models.ForeignKey(User, db_index=True)
+    task_state = models.CharField(max_length=16, choices=TASK_STATE, default='pending')
+    period_start = models.DateTimeField()
+    period_end = models.DateTimeField()
+    exam_time_limit = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(1440)])
+    problem_order = models.BooleanField(default=False)
+    show_answer = models.BooleanField(default=False)
+
+
+class ExamParticipant(TimeStampedModel):
+
+    exam_task = models.ForeignKey(ExamTask, related_name='participant')
+    participant = models.ForeignKey(User)
+    exam_result = models.CharField(max_length=16, choices=EXAM_RESULT)
+    participate_time = models.DateTimeField()
+    hand_in_time = models.DateTimeField()
+
+    @property
+    def total_grade(self):
+        result = self.answer.aggregate(total_grade=Sum('grade'))
+        return result and result.get('total_grade') or 0
+
+
+class ExamParticipantAnswer(TimeStampedModel):
+
+    participant = models.ForeignKey(ExamParticipant, related_name='answer')
+    problem_id = models.CharField(max_length=255, db_index=True)
+    answer = models.TextField()
+    grade = models.DecimalField(max_digits=5, decimal_places=2,
+                                validators=[
+                                    MinValueValidator(Decimal((0, (0, 0, 1), -2))),
+                                    MaxValueValidator(Decimal(100))])
+
+
+class ExamPaperProblemsSnapShot(TimeStampedModel):
+    exam_paper = models.ForeignKey(ExamTask, related_name='problems', null=True)
+    sequence = models.CharField(max_length=16, default='01')
+    problem_id = models.CharField(max_length=255, db_index=True)
+    problem_type = models.CharField(max_length=16, choices=PROBLEM_TYPE)
+    grade = models.DecimalField(max_digits=5, decimal_places=2,
+                                validators=[
+                                    MinValueValidator(Decimal((0, (0, 0, 1), -2))),
+                                    MaxValueValidator(Decimal(100))])
+    content = JSONField()
