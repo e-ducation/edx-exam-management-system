a6e42e08c72692643108458c65a451a4b458131a
weijie <wejie00@foxmail.com>
2018-09-26 11:48:39 +0800

    Update flake8 check

--- exam_paper/models.py
+++ exam_paper/models.py
@@ -6,7 +6,7 @@ from decimal import Decimal
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models import Sum, F, Count, Q, Case, When
+from django.db.models import Sum, F, Count
 
 from model_utils.models import TimeStampedModel
 from jsonfield import JSONField
