4f1c3ddc606ed70c82fa4e98a354e8a3a93d78b3
ufresh2013 <ufresh2013@163.com>
2018-09-27 14:32:19 +0800

    作答交互

--- exam-frontend/src/page/Exam/index.js
+++ exam-frontend/src/page/Exam/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon, Radio, Checkbox, Input, Button, message, Spin, Breadcrumb } from 'antd';
+import { Icon, Radio, Checkbox, Input, Button, message, Spin, Breadcrumb, Modal} from 'antd';
 import Footer from '../../components/Footer';
 import HeaderStudent from '../../components/HeaderStudent';
 import axios from 'axios';
@@ -7,71 +7,143 @@ import './index.scss';
 import $ from "jquery";
 
 
-class ExamingContainer extends React.Component {
+class ExamContainer extends React.Component {
   constructor(props) {
     super(props);
     this.timer = null;
+    this.participant_id = null;
   }
 
   state = {
-    name: '',
-    total_problem_num: null,
-    total_grade: null,
-    passing_grade: null,
-    description: '',
-    problems: [],
-    loading: true,
+    /* mode = started 考试中 / finished 考试结束 / submit_success 显示试卷提交成功 / error 显示错误信息 */
+    mode: 'started',
+    exam_task: {},
+    results: [],
     timestamp_end: null,
     timestamp_now: null,
-    showExaming: false,
-    showSubmit: true,
-    showPreview: false,
-    showError: false,
+    loading: false,
+    /* unFillArr 保存的是 index + 1 */
+    unFillArr: [],
   }
 
   componentDidMount() {
-    // 获取试卷id
-    // 获取试卷内容
-    const id = window.location.href.split('/examing/')[1];
-    const that = this;
+    // 获取试卷participant_id
+    const id = window.location.href.split('/exam/')[1];
 
+    /* 考试中，请求成功 */
+    const res = {
+      "status": 0,
+      "message": "",
+      "data":{
+        "participant_id": 1,
+        "task_state": "started", //考试状态
+        "current_time": "2018-09-27 14:19:50",//当前时间
+        "exam_task": {
+            "id": 3,//考试任务ID
+            "name": "运营管理第三次考试", //考试任务名称
+            "exampaper_description": "运营管理3运营管理3运营管理3运营管理3运营管理3运营管理3运营管理3运营管理3运营管理3运营管理3运营管理3运营管理3运营管理3",//考试说明
+            "creator": "edx",//发布人
+            "period_start": "2018-09-26 18:00:00",//考试开始时间
+            "period_end": "2018-09-27 14:20:00",//考试结束时间
+            "exam_time_limit": 60,//考试时长
+            "exampaper_passing_ratio": 60,//及格线
+            "exampaper_total_problem_num": 6,//总问题数
+            "exampaper_total_grade": "6.00"//试卷总分
+        },
+        "results": [
+          {
+            "id": 1,
+            "answer": '', //用户答案 多选[0,1,2,3,4]
+            "grade": "0.00", //用户得分
+            "problem_grade": "1.00", //题目分数
+            "sequence": "3", //序号
+            "problem_type": "multiplechoiceresponse", //题目类型
+            "content": {
+              'title': '要坚持无进去、全服的方式打开了就分手了的看法吉林省乐山大佛记录卡士大夫娄底市解放路看到了里是否看电视剧',
+              'descriptions': {},
+              'options': ['1.22', '1.66', '2.34', '3.33', '4.44']
+            },
+          },
+          {
+            "id": 2,
+            "answer": [0], //用户答案 多选[0,1,2,3,4]
+            "grade": "0.00", //用户得分
+            "problem_grade": "1.00", //题目分数
+            "sequence": "3", //序号
+            "problem_type": "choiceresponse", //题目类型
+            "content": {
+              'title': '要坚持无进去、全服的方式打开了就分手了的看法吉林省乐山大佛记录卡士大夫娄底市解放路看到了里是否看电视剧',
+              'descriptions': {},
+              'options': ['1.22', '1.66', '2.34', '3.33', '4.44']
+            },
+          },
+          {
+            "id": 3,
+            "answer":"the correct answer",
+            "problem_id":"yingli+ceshi005+2020+type@problem+block@7655160fbf8d43d09cfe447be3c08e13",
+            "problem_grade":"1.00",
+            "sequence":"0",
+            "problem_type":"stringresponse",
+            "content":{
+              "descriptions":"You can add an optional tip or note related to the prompt like this. ",
+              "title":"Add the question text, or prompt, here. This text is ."
+            }
+          }
+        ]
+      }
+    }
+
+    const { participant_id, task_state, current_time, exam_task, results } = res.data;
+    this.setState({
+      mode: task_state,
+      exam_task,
+      results,
+      timestamp_end: Date.parse(new Date(exam_task.period_end)),
+      timestamp_now: Date.parse(new Date(current_time)),
+      loading: false,
+    }, () => {
+
+      // 设置倒计时
+      this.timer = setInterval(() => {
+        const { timestamp_end, timestamp_now } = this.state;
+        if (timestamp_now - timestamp_end > -1) {
+          // 自动交卷，清除倒计时
+          clearInterval(this.timer);
+          this.setState({
+            mode: 'submit_success'
+          })
+          return false;
+        }
+        this.setState({
+          timestamp_now: timestamp_now + 1000,
+        });
+
+        // 倒计时少于0秒，自动交卷
+      }, 1000)
+    });
+    this.participant_id = participant_id;
+
+    /*
     axios.get('/api/my_exam/exam_task/exam_answers?participant_id=' + id)
       .then((response) => {
         const res = response.data;
 
         // 可以进行考试
         if (res.status === 0) {
-          const { name, passing_grade, problems, total_grade, total_problem_num, description, create_type } = res.data;
-          this.setState({
-            name,
-            passing_grade,
-            problems,
-            total_grade,
-            total_problem_num,
-            description,
-            loading: false,
-            timestamp_end: Date.parse(new Date('2018/09/20'))
-          })
-          // 设置倒计时
-          that.timer = setInterval(() => {
-            const { timestamp_end, timestamp_now } = that.state;
-            if (timestamp_now > timestamp_end) {
-              clearInterval(this.timer);
-              return false;
-            }
-            that.setState({
-              timestamp_now: timestamp_now + 1000,
-            });
-          }, 1000)
+
 
         } else {
           message.error('请求失败');
         }
       })
-      .catch(function (error) {
+      .catch((error) => {
         message.error('请求失败')
       });
+      */
+  }
 
+  componentWillUnmount(){
+    clearInterval(this.timer)
   }
 
   backToTop() {
@@ -89,16 +161,104 @@ class ExamingContainer extends React.Component {
     window.location.href = "/#/student_manage?tab=1"
   }
 
+  // 更改单选题答案
+  onChangeRadioAnswer = (index, e) => {
+    this.setAnswer(index, e.target.value);
+  }
+
+  // 更改多选题答案
+  onChangeCheckboxAnswer = (index, values) => {
+    this.setAnswer(index, values);
+  }
+
+  // 更改选择题答案
+  onChangeInputAnswer = (index, e) => {
+    this.setAnswer(index, e.target.value);
+  }
+
+  setAnswer = (index, val) => {
+    let { results, unFillArr } = this.state;
+    results[index].answer = val;
+
+
+    // 如果题目边框红色，去掉颜色, unFillArr存储的是
+    const a = unFillArr.indexOf(index);
+    if (a > -1){
+      unFillArr.splice(a, 1)
+    }
+
+    this.setState({
+      results,
+      unFillArr,
+    });
+  }
+
+
+  // 点击交卷
+  onClickSubmit = () => {
+    // 检查选项是否为空
+    const { results } = this.state;
+    let unFillArr = [];
+    for (let i = 0; i < results.length; i++){
+      if (!results[i].answer || results[i].answer.length === 0){
+        unFillArr.push(i);
+      }
+    }
+
+    if (unFillArr.length > 0){
+      // 有选项非空
+      this.setState({
+        unFillArr,
+      })
+
+      const strArr = unFillArr.map((item) => { return item + 1});
+      Modal.confirm({
+        iconType: 'exclamation-circle',
+        title: '提示',
+        content: '第【' + strArr.join(', ') + '】题未答题，交卷后将无法修改答案，确定交卷？',
+        okText: '确定',
+        cancelText: '取消',
+        onOk: () => {
+          this.submit();
+        }
+      });
+      return false;
+    } else {
+      // 选项都填上了
+      // 二次确认进项交卷
+      Modal.confirm({
+        iconType: 'exclamation-circle',
+        title: '提示',
+        content: '交卷后将无法修改答案，确定交卷？',
+        okText: '确定',
+        cancelText: '取消',
+        onOk: () => {
+          this.submit();
+        }
+      });
+    }
+
+  }
+
+  // 交卷
+  submit = () => {
+    console.log('ajax submit');
+    this.setState({
+      mode: 'submit_success'
+    })
+  }
 
   render() {
     // multiplechoiceresponse 单选题
     // choiceresponse         多选题
     // stringresponse         填空题
-    const { name, passing_grade, problems, total_grade, total_problem_num, description, loading, showExaming, showSubmit, showPreview, showError } = this.state;
-    const leftTime = this.state.timestamp_end - this.state.timestamp_now;
+    const { mode, exam_task, results, timestamp_end, timestamp_now, loading, unFillArr } = this.state;
+    const leftTime = timestamp_end - timestamp_now;
     const hours = this.checkTime(parseInt(leftTime / 1000 / 60 / 60 % 24, 10)); //计算剩余的小时
     const minutes = this.checkTime(parseInt(leftTime / 1000 / 60 % 60, 10));//计算剩余的分钟
     const seconds = this.checkTime(parseInt(leftTime / 1000 % 60, 10));//计算剩余的秒数
+    const timeStyle = leftTime < 1000 * 60 * 10 ? { color: '#f5222d' } : null;
+
 
     return (
       <div style={{ width: '100%', wordBreak: 'break-word' }}>
@@ -137,19 +297,19 @@ class ExamingContainer extends React.Component {
                   // 2. 考试结束，提交成功
                   // 3. 考试结束，显示答卷
                   // 4. 错误信息提示框 : 未开始考试 / 找不到试卷
-                  if (showExaming) {
+                  if (mode === 'started') {
                     // 1. 正在考试
                     return (
                       <div>
                         <div className="preview-block" style={{ textAlign: 'center' }}>
-                          <h1 style={{ fontSize: '16px' }}>{name}</h1>
-                          <h2 style={{ margin: '20px 0' }}>共{total_problem_num}题目<span style={{ margin: '0 10px' }}>试题总分: {total_grade}分</span>及格分: {passing_grade}分</h2>
-                          <p style={{ textAlign: 'left' }}>{description}</p>
+                          <h1 style={{ fontSize: '16px' }}>{exam_task.name}</h1>
+                          <h2 style={{ margin: '20px 0' }}>共{exam_task.exampapaer_total_problem_num}题目<span style={{ margin: '0 10px' }}>试题总分: {exam_task.exampaper_total_grade}分</span>及格分: {exam_task.exampaper_passing_ratio}分</h2>
+                          <p style={{ textAlign: 'left' }}>{exam_task.exampaper_description}</p>
                         </div>
                         {
-                          problems.map((item, index) => {
+                          results.map((item, index) => {
                             return (
-                              <div className="preview-block" key={index}>
+                              <div className="preview-block" key={item.id} style={unFillArr.indexOf(index) > -1 ? {border:'1px solid red'} : null }>
                                 <div>
                                   <p className="preview-title">
                                     {index + 1}.
@@ -170,14 +330,14 @@ class ExamingContainer extends React.Component {
                                       }
                                     </span>
                                     {item.content.title}
-                                    （{item.grade}分）
+                                    （{item.problem_grade}分）
                                 </p>
                                   <div style={{ marginLeft: '18px' }}>
                                     {
                                       (() => {
                                         switch (item.problem_type) {
                                           case 'multiplechoiceresponse':
-                                            return <Radio.Group style={{ display: 'block' }} defaultValue={null}>
+                                            return <Radio.Group style={{ display: 'block' }} value={item.answer} onChange={this.onChangeRadioAnswer.bind(this, index)}>
                                               {
                                                 item.content.options.map((item, index) => {
                                                   return <Radio style={{ display: 'block', lineHeight: '1.5', whiteSpace: 'pre-wrap', margin: '0 0 10px 0' }} key={index} value={index}>{item}</Radio>
@@ -186,7 +346,7 @@ class ExamingContainer extends React.Component {
                                             </Radio.Group>;
 
                                           case 'choiceresponse':
-                                            return <Checkbox.Group defaultValue={null}>
+                                            return <Checkbox.Group defaultValue={null} value={item.answer} onChange={this.onChangeCheckboxAnswer.bind(this, index)}>
                                               {
                                                 item.content.options.map((item, index) => {
                                                   return <Checkbox style={{ display: 'block', lineHeight: '1.5', whiteSpace: 'pre-wrap', margin: '0 0 10px 0' }} key={index} value={index}>{item}</Checkbox>
@@ -201,6 +361,8 @@ class ExamingContainer extends React.Component {
                                                 autosize={{ minRows: 1, maxRows: 6 }}
                                                 style={{ width: '385px', display: 'inline-block', marginLeft: '15px', verticalAlign: 'text-top' }}
                                                 maxLength="2000"
+                                                value={item.answer}
+                                                onChange={this.onChangeInputAnswer.bind(this, index)}
                                               />
                                             </div>
 
@@ -218,9 +380,9 @@ class ExamingContainer extends React.Component {
                         <div className="exam-count">
                           <p style={{ marginTop: '14px' }}>距离考试结束还有</p>
                           <div>
-                            <span className="exam-count-time">{hours}</span>
-                            <span className="exam-count-time">{minutes}</span>
-                            <span className="exam-count-time">{seconds}</span>
+                            <span className="exam-count-time" style={timeStyle}>{hours}</span>
+                            <span className="exam-count-time" style={timeStyle}>{minutes}</span>
+                            <span className="exam-count-time" style={timeStyle}>{seconds}</span>
                           </div>
                           <div>
                             <span className="exam-count-text">时</span>
@@ -229,11 +391,11 @@ class ExamingContainer extends React.Component {
                           </div>
                         </div>
                         <div style={{ textAlign: 'center', margin: '20px 0' }}>
-                          <Button type="primary">交卷</Button>
+                          <Button type="primary" onClick={this.onClickSubmit}>交卷</Button>
                         </div>
                       </div>
                     )
-                  } else if (showSubmit) {
+                  } else if (mode === 'submit_success') {
                     // 2. 考试结束，提交成功
                     return (
                       <div className="exam-msg-block">
@@ -244,9 +406,9 @@ class ExamingContainer extends React.Component {
                         <Button type="primary">查看成绩</Button>
                       </div>
                     )
-                  } else if (showPreview) {
+                  } else if (mode === 'finished') {
                     // 3. 考试结束，显示答卷
-                  } else if (showError) {
+                  } else if (mode === 'error') {
                     // 4. 错误信息提示框
 
                   }
@@ -261,7 +423,7 @@ class ExamingContainer extends React.Component {
 
 
 
-export default class Examing extends React.Component {
+export default class Exam extends React.Component {
   state = {
     height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeigh,
     showBackToTop: false,
@@ -289,7 +451,7 @@ export default class Examing extends React.Component {
       <div>
         <HeaderStudent changeUpBtn={this.onChangeUpBtn} />
         <div className="container container-examing" style={containerHeight}>
-          <ExamingContainer showBackToTop={this.state.showBackToTop} />
+          <ExamContainer showBackToTop={this.state.showBackToTop} />
         </div>
         <Footer />
       </div>
